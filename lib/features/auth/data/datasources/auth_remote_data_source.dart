1bd89819ea87c68d28db6efd404a145ff1a9df03
Khuzaima <khuzaimaansari2004@gmail.com>
2024-03-26 20:31:12 +0530

    Completed Auth Process

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -21,9 +21,19 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   Future<UserModel> logininWithEmailPassword({
     required String email,
     required String password,
-  }) {
-    // TODO: implement logininWithEmailPassword
-    throw UnimplementedError();
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signInWithPassword(
+        password: password,
+        email: email,
+      );
+      if (response.user == null) {
+        throw ServerException("User is null !");
+      }
+      return UserModel.fromJson(response.user!.toJson());
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
   }
 
   @override

dcb3828d478fb443e0df63e12155a82a501671ea
Khuzaima <khuzaimaansari2004@gmail.com>
2024-03-26 16:20:13 +0530

    Created Model and Entities for Auth

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,8 +15,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       final res = await _userSignUp(UserSignUpParams(
           name: event.name, email: event.email, password: event.password));
 
-      res.fold(
-          (l) => emit(AuthFailure(l.message)), (r) => emit(AuthSuccess(r)));
+      res.fold((l) => emit(AuthFailure(l.message)),
+          (user) => emit(AuthSuccess(user)));
     });
   }
 }
